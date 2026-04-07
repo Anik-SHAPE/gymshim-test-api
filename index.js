9de59905250758ed60dda84b7282e4f3669cfc66
anik <anik.shape@gmail.com>
2026-04-07 21:26:50 +0530

    Api Update

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
-const PORT = 3000;
+app.use(cors());
 
-app.use(express.json());
+const PORT = 8000;
 
-app.get("/", (req, res) => {
-  res.send("Backend is running 🚀");
+app.get("/api/message", (req, res) => {
+  res.json({ message: "Hello from backend 🚀" });
 });
 
 app.listen(PORT, () => {
