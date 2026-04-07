704a90823722681a1e7fe87de16e0101a82ad229
Anik Roy <101314480+Anik-SHAPE@users.noreply.github.com>
2026-04-07 20:26:32 +0000

    express json permission update

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
 
 const PORT = 8000;
+
+app.use(cors({
+  origin: "*"
+}));
 
 app.get("/api/message", (req, res) => {
   res.json({ message: "Hello from backend 🚀" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(8000, "0.0.0.0", () => {
+  console.log("Server running on port 8000");
 });
